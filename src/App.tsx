3764a50a456948d845f9c59f02a83d797958fcc8
John Clampet <jsclampet@gmail.com>
2023-10-21 23:24:49 -0500

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,56 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [starWarsData, setStarWarsData] = useState([]);
+  const defaultPlanets = localStorage.getItem("planets") || [];
+  const defaultSpecies = localStorage.getItem("species") || [];
+  const defaultCharacters = JSON.parse(localStorage.getItem("character")) || [];
+
+  const [planets, setPlanets] = useState(defaultPlanets);
+  const [species, setSpecies] = useState(defaultSpecies);
+  const [characters, setCharacters] = useState(defaultCharacters);
+
   useEffect(() => {
-    axios
-      .get("https://swapi.dev/api/people/?page=1")
-      .then((data) => setStarWarsData(data.data.results));
+    axios.get("https://swapi.dev/api/people/?page=1").then((data) => {
+      setCharacters(data.data.results);
+    });
   }, []);
 
+  useEffect(() => {
+    const controller = new AbortController();
+
+    let planetsArray = [];
+    characters.forEach((character) => {
+      const planetReformatted: string = character.homeworld
+        .split("/")
+        .slice(4, 6)
+        .join("");
+      if (!Object.values(planetsArray).includes(character.homeworld)) {
+        const controller = new AbortController();
+        axios.get(character.homeworld).then(({ data }) => {
+          planetsArray = [
+            ...planetsArray,
+            { planetReformatted: data.name, url: character.homeworld },
+          ];
+          planetsArray.forEach((planet) => console.log(planet));
+          return controller.abort();
+        });
+      }
+    });
+    return controller.abort();
+  }, [characters]);
+
+  // // // // // // // // // // // // // // // //
+
+  const formatBirthYear = (character) => {
+    return character.birth_year == "unknown"
+      ? character.birth_year
+      : character.birth_year.split("B")[0] + " BBY";
+  };
+
   return (
-    <div>
-      <h1>StarWars</h1>
-      <input type="text" />
+    <div className="p-5">
+      <h1 className="mb-2 text-center">StarWars</h1>
+      <input type="text" id="search" alt="Search Bar" className="mb-4" />
       <table className="table table-bordered">
         <thead>
           <tr>
@@ -25,14 +64,14 @@ const App = () => {
           </tr>
         </thead>
         <tbody>
-          {starWarsData.length > 0 &&
-            starWarsData.map((character) => {
+          {characters.length > 0 &&
+            characters.map((character) => {
               return (
-                <tr>
+                <tr key={crypto.randomUUID()}>
                   <td>{character.name}</td>
-                  <td>{character.birth_year}</td>
-                  <td>{character.height}</td>
-                  <td>{character.mass}</td>
+                  <td>{formatBirthYear(character)}</td>
+                  <td>{character.height} cm</td>
+                  <td>{character.mass} kg</td>
                   <td>{}</td>
                   <td>{}</td>
                 </tr>
