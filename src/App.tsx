1db4fb278ae28c91848ee82cc991205df111f390
John Clampet <jsclampet@gmail.com>
2023-10-27 20:42:38 -0500

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import "./App.css";
 import userService from "./services/user-service";
 
 const App = () => {
-  const [planets, setPlanets] = useState([]);
-  const [species, setSpecies] = useState([]);
   const [characters, setCharacters] = useState([]);
   const [isLoading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
@@ -45,122 +43,112 @@ const App = () => {
     }
     getCharacters();
     // for (const character of characters) {
-    //   // get species data
+    //   get species data
     //   const speciesResponse = await axios.get(character.species[0]);
     //   character.speciesName = speciesResponse.data.name;
-    //   // get planert data
+    //   get planert data
     //   const planetResponse = await axios.get(character.homeworld);
     //   character.homeworldName = planetResponse.data.name;
+    setLoading(false);
     return () => controller.abort();
   }, [page]);
 
-  return "hello";
-  // return (
-  //   <div className="p-5 pt-0 main-bg">
-  //     <img
-  //       src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Star_Wars_Logo..png/640px-Star_Wars_Logo..png"
-  //       alt=""
-  //     />
-  //    {isLoading && (
-  //         <h1 className="text-light text-center">Loading, please wait!</h1>
-  //       )}
-  //       <div className={isLoading ? "hidden" : "visible"}>
-  //         <form action="" className="">
-  //           <div className="form-group mb-4">
-  //             <input
-  //               placeholder="Search by keyword"
-  //               type="search"
-  //               id="search"
-  //               alt="Search Bar"
-  //               className=" bg-transparent"
-  //             />
-  //             <button className="btn btn-secondary " type="submit">
-  //               Search
-  //             </button>
-  //           </div>
-  //         </form>
-  //         <table className="table table-bordered mb-4">
-  //           <thead>
-  //             <tr>
-  //               <th>Name</th>
-  //               <th>Birth Date</th>
-  //               <th>Height</th>
-  //               <th>Mass</th>
-  //               <th>Home World</th>
-  //               <th>Species</th>
-  //             </tr>
-  //           </thead>
-  //           <tbody>
-  //             {characters.length > 0 &&
-  //               planets.length > 0 &&
-  //               species.length > 0 &&
-  //               characters.map((character) => {
-  //                 return (
-  //                   <tr key={crypto.randomUUID()}>
-  //                     <td>{character.name}</td>
-  //                     <td>{formatBirthYear(character)}</td>
-  //                     <td>{character.height} cm</td>
-  //                     <td>
-  //                       {character.mass === "unknown"
-  //                         ? "unknown"
-  //                         : character.mass + " kg"}
-  //                     </td>
-  //                     <td>
-  //                       {planets.length > 0 &&
-  //                         // ADDING STRINGIFY TO SEE WHAT IS OBJECTS ARE BEING RETURNED
-  //                         // WITHOUT STRINGIFY, CODE BREAKS:
-  //                         //    --- ERROR MESSAGE name is undefined
+  return (
+    <div className="p-5 pt-0 main-bg">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Star_Wars_Logo..png/640px-Star_Wars_Logo..png"
+        alt=""
+      />
 
-  //                         planets.filter(
-  //                           (planet) =>
-  //                             planet.url === character.homeworld.split("api")[2]
-  //                         )[0].name}
-  //                     </td>
-  //                     <td>{character.speciesName}</td>
-  //                   </tr>
-  //                 );
-  //               })}
-  //           </tbody>
-  //         </table>
-  //         <nav aria-label="Page navigation">
-  //           <ul className="pagination">
-  //             <li className="page-item">
-  //               <a
-  //                 className="page-link"
-  //                 onClick={() => (page > 1 ? setPage((prev) => prev - 1) : page)}
-  //                 href="#"
-  //               >
-  //                 Previous
-  //               </a>
-  //             </li>
-  //             <li className="page-item">
-  //               <a className="page-link" href="#">
-  //                 1
-  //               </a>
-  //             </li>
-  //             <li className="page-item">
-  //               <a className="page-link" href="#">
-  //                 2
-  //               </a>
-  //             </li>
-  //             <li className="page-item">
-  //               <a className="page-link" href="#">
-  //                 3
-  //               </a>
-  //             </li>
-  //             <li className="page-item">
-  //               <a
-  //                 className="page-link"
-  //                 href="#"
-  //                 onClick={() => setPage(page + 1)}
-  //               >
-  //                 Next
-  //               </a>
-  //             </li>
-  //           </ul>
-  //         </nav>
-  //       </div>
-  //    </div>
+      {isLoading && (
+        <h1 className="text-light text-center">Loading, please wait!</h1>
+      )}
+      <div className={isLoading ? "hidden" : "visible"}>
+        <form action="" className="">
+          <div className="form-group mb-4">
+            <input
+              placeholder="Search by keyword"
+              type="search"
+              id="search"
+              alt="Search Bar"
+              className=" bg-transparent"
+            />
+            <button className="btn btn-secondary " type="submit">
+              Search
+            </button>
+          </div>
+        </form>
+
+        <table className="table table-bordered mb-4">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Birth Date</th>
+              <th>Height</th>
+              <th>Mass</th>
+              <th>Home World</th>
+              <th>Species</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {characters.length > 0 &&
+              planets.length > 0 &&
+              species.length > 0 &&
+              characters.map((character) => {
+                return (
+                  <tr key={crypto.randomUUID()}>
+                    <td>{character.name}</td>
+                    <td>{formatBirthYear(character)}</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+        <nav aria-label="Page navigation">
+          <ul className="pagination">
+            <li className="page-item">
+              <a
+                className="page-link"
+                onClick={() => (page > 1 ? setPage((prev) => prev - 1) : page)}
+                href="#"
+              >
+                Previous
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="#">
+                1
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="#">
+                2
+              </a>
+            </li>
+            <li className="page-item">
+              <a className="page-link" href="#">
+                3
+              </a>
+            </li>
+            <li className="page-item">
+              <a
+                className="page-link"
+                href="#"
+                onClick={() => setPage(page + 1)}
+              >
+                Next
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default App;
